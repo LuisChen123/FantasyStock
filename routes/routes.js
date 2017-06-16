8ec7d324a550c88b35faa2f745ade7ef522d2051
Zihao Li <Zihao Li>
2017-06-16 09:42:46 -0700

    added signUp, logIn routes to routes.js in routes folder

--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,26 @@
 // import dependencies 
 var express = require("express");
 var path = require("path"); 
+
 // import other route groups
 var apiRoutes = require("./apiRoutes");
 // create new instance of router
 var router = new express.Router(); 
 
-//if route starts with api, use apiRoutes module
+//	This route deals with anything interacting with the database.
 router.use("/api", apiRoutes);
+//	this route is for verifying user login info
+router.post("/logIn", function(req, res){
 
-// send all other requests this
+});
+// This route is for dealing with new user sign up
+router.post("/signUp", function(req, res){
+
+}); 
+
+// send all other requests this. This will give the user the login page. 
 router.get("*", function(req, res){
-	res.sendFile(path.join(__dirname, "../public/index.html"));
+	res.sendFile(path.join(__dirname, "../public/loginPage.html"));
 });
 
 module.exports = router; 
