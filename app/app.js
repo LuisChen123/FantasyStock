479c0b51d48f0d41387a707719099e01edb69903
Clinton Diamond <clintondiamond@ClintonmondsMBP.att.net>
2017-06-15 22:27:41 -0700

    first commit for react component

---
+++ app/app.js
@@ -0,0 +1,9 @@
+// Include the Main React Dependencies
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// Include the main Main Component
+var routes = require("./config/routes");
+
+// This code here allows us to render our main component (in this case Main)
+ReactDOM.render(routes, document.getElementById("app"));
