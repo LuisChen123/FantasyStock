724fb87c96526c4f572f7defbf03c6af81d98a94
Zihao Li <Zihao Li>
2017-06-17 09:31:21 -0700

    footer is done

--- app/components/Main.js
+++ app/components/Main.js
@@ -3,6 +3,7 @@ import React, {Component} from "react";
 // Including the Link component from React Router to navigate within our application without full page reloads
 import { Link } from "react-router";
 import navbar from "./navbar.js"; 
+import footer from "./footer.js";
 import apiHelper from "../apiHelper/apiHelper.js"; 
 
 class Main extends Component {
@@ -27,7 +28,7 @@ class Main extends Component {
       <div className = "container"> 
         <navbar /> 
           {props.children}
-        
+        <footer /> 
       </div> 
     ); 
   }
