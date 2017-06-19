eaa840466a0b43aaca77892ceaf77c688c68c3c9
Zihao Li <Zihao Li>
2017-06-19 11:44:02 -0700

    added bcrypt code, needs to be tested

--- models/dbManager.js
+++ models/dbManager.js
@@ -1,15 +1,57 @@
 // bringing articleModel in
 var userModel = require("./user.js"); 
+var bcrypt = require("bcryptjs"); 
+var path = require("path");
 // create single dbManger
 var dbManager = {
-	getUserInfo: function(cb){
+
+	userModel_GetUserInfo: function(cb){
 		userModel.find({}, function(err, info){
 			if(err){
 				console.log(err);
 			}
 			cb(info); 
 		});
-	}
+	} 
+
+	userModel_SaveUser: function(req, res) {
+    	var body = req.body;
+    	bcrypt.genSalt(10, function(err, salt) {
+      		bcrypt.hash(body.password, salt, function(err, hash) {
+        		// Store hash in your password DB.
+        		userModel.create({
+          			username: body.name,
+          			loginPassword: hash,
+          			firstName: body.firstName,
+          			lastName: body.lastName,
+          			age: body.age,
+          			grade: body.grade,
+          			email: body.email
+        		})
+        		.then(function(err, user) {
+        			if(err){
+        				res.json(err);
+        			}
+          			else{
+          				res.sendFile(path.join(__dirname,"../public/react.html"));
+          			}
+        		});
+      		});
+    	});
+  	}
+
+  	userModel_AuthenticateUser: function(req, res) {
+    //query for password where username = req.body.username
+    	bcrypt.compare(req.body.password, queryResults, function(err, bcryptRes) {
+      		if (bcryptRes) {
+        		res.sendFile(path.join(__dirname,"../public/react.html"));
+      		} 
+      		else {
+        		res.json(false);
+      		}
+    	});
+  	}
+
 	/*
 	// will check to see if user has the money to make the purchase, if so then user info will be updated,
 	// if not, user will be alerted. 
@@ -30,7 +72,7 @@ var dbManager = {
 				cb("You Dont Have Enough Cash To Make This Purchase");
 			}
 		});
-		*/
+	
 	}, 
 	// Users_InsertNew: function(articleID, url, cb){
 	// 	articleModel.create({
@@ -61,7 +103,7 @@ var dbManager = {
 	// 		cb(info);
 	// 	});
 	// }
-	
+	*/
 }
 
 module.exports = dbManager; 
