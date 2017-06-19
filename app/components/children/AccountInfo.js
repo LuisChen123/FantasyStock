e3c36af6eaa051f3389538dd750c4d6d16a80884
Clinton Diamond <clintondiamond@Clinton-Diamonds-MacBook-Pro.local>
2017-06-19 10:35:37 -0700

    upaged the accounting info page

---
+++ app/components/children/AccountInfo.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class Info extends Component {
+  constructor() {
+    super();
+  
+  }
+
+
+  render() {
+    return (
+        <div className="panel-body text-center">
+          {/* Here we use a map function to loop through an array in JSX */}
+          <ul className="list-unstyled">
+            {this.props.results.map((item, i)=> {
+            return (
+              <li key ={i} className="col-md-2"> 
+                  <p>{item.stockName}</p>
+                  <p> {item.shareCount}</p>
+              </li>
+            );
+          })}
+       </ul>
+      </div>
+    );
+  }
+}
+
+export default Home;
