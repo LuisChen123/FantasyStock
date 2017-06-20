4821c156e9711951d6bb3c9ae39710bc37ff823c
Zihao Li <Zihao Li>
2017-06-20 16:06:26 -0700

    made changes to trade page

--- app/components/Trade.js
+++ app/components/Trade.js
@@ -17,114 +17,114 @@ class Trade extends Component {
     			stockTicker : "AXP",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "BA",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "CAT",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "CSCO",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "CVX",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "DD",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "DIS",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "GE",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "GNG",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "GS",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "HD",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "IBM",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "INTC",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "JPM",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "KO",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "MCD",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "MRK",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "MSFT",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "NKE",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "PFE",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "PG",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "TRV",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "UNH",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "UTX",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "V",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "VZ",
     			currentPrice : 0
     		},
-        {
+        	{
     			stockTicker : "WMT",
     			currentPrice : 0
     		},
-        {
-          stockTicker: "XOM",
-          currentPrice: 0
-        }      
+        	{
+          		stockTicker: "XOM",
+          		currentPrice: 0
+        	}      
     	]
     };
 
@@ -146,7 +146,7 @@ class Trade extends Component {
       var sortedResponse = unsortedResponse.sort(); 
       this.setState({
         DowJonesArray: 
-          [
+          	[
     		    {
     			  // stockTicker: "AAPL",
     			  currentPrice: 0
@@ -155,114 +155,114 @@ class Trade extends Component {
     			  // stockTicker : "AXP",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "BA",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "CAT",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "CSCO",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "CVX",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "DD",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "DIS",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "GE",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "GNG",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "GS",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "HD",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "IBM",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "INTC",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "JPM",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "KO",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "MCD",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "MRK",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "MSFT",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "NKE",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "PFE",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "PG",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "TRV",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "UNH",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "UTX",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "V",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "VZ",
     			  currentPrice : 0
     		    },
-            {
+            	{
     			  // stockTicker : "WMT",
     			  currentPrice : 0
     		    },
-            {
-            // stockTicker: "XOM",
-            currentPrice: 0
-            }      
+            	{
+            	// stockTicker: "XOM",
+            	currentPrice: 0
+            	}      
     	    ]
       });
     })
