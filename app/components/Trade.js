a6154b750c1e19774f3bb1dabf7fde67f70a73a0
Zihao Li <Zihao Li>
2017-06-21 12:26:39 -0700

    got api working

--- app/components/Trade.js
+++ app/components/Trade.js
@@ -5,286 +5,88 @@ import apiHelper from "../apiHelper/apiHelper.js";
 class Trade extends Component {
   constructor() {
     super();
-    this.state = 
-    {
-    	DowJonesArray : 
-    	[
-    		{
-    			stockTicker: "AAPL",
-    			currentPrice: 0
-    		},
-    		{
-    			stockTicker : "AXP",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "BA",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "CAT",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "CSCO",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "CVX",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "DD",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "DIS",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "GE",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "GNG",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "GS",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "HD",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "IBM",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "INTC",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "JPM",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "KO",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "MCD",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "MRK",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "MSFT",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "NKE",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "PFE",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "PG",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "TRV",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "UNH",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "UTX",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "V",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "VZ",
-    			currentPrice : 0
-    		},
-        	{
-    			stockTicker : "WMT",
-    			currentPrice : 0
-    		},
-        	{
-          		stockTicker: "XOM",
-          		currentPrice: 0
-        	}      
-    	]
-    };
+    this.state = { DowJonesArray: [], interval: "" };
 
     this.getDowPrice = this.getDowPrice.bind(this);
 
   }
 
   componentDidMount() {
-    this.getDowPrice(); 
+    this.getDowPrice();
+    var intervalId = setInterval(this.getDowPrice, 3000);
+    this.setState({interval: intervalId});
   }
   
+  componentWillUnmount() {
+   	// use intervalId from the state to clear the interval
+   	clearInterval(this.state.interval);
+  }
+
   getDowPrice(){
     apiHelper.getDowPrice()
     .then((response) =>{
+      console.log("line 21"); 
+      var unsortedStringResponse = response.data.slice(4, response.data.length); 
+      var parsedUnsortedResponse = JSON.parse(unsortedStringResponse);
+      console.log(parsedUnsortedResponse); 
       var unsortedResponse = [];
-      for(var x=0; x<response.length; x++){
-        unsortedResponse.push({ stockName: response.t, stockPrice: parseInt(response.l) });
+      for(var x=0; x<parsedUnsortedResponse.length; x++){
+        unsortedResponse.push({stockName: parsedUnsortedResponse[x].t, stockPrice: parseFloat(parsedUnsortedResponse[x].l)});
       }
-      var sortedResponse = unsortedResponse.sort(); 
-      this.setState({
-        DowJonesArray: 
-          	[
-    		    {
-    			  // stockTicker: "AAPL",
-    			  currentPrice: 0
-    		    },
-    		    {
-    			  // stockTicker : "AXP",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "BA",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "CAT",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "CSCO",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "CVX",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "DD",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "DIS",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "GE",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "GNG",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "GS",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "HD",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "IBM",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "INTC",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "JPM",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "KO",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "MCD",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "MRK",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "MSFT",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "NKE",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "PFE",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "PG",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "TRV",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "UNH",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "UTX",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "V",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "VZ",
-    			  currentPrice : 0
-    		    },
-            	{
-    			  // stockTicker : "WMT",
-    			  currentPrice : 0
-    		    },
-            	{
-            	// stockTicker: "XOM",
-            	currentPrice: 0
-            	}      
-    	    ]
-      });
+      console.log("line 29, Trade.js"); 
+      var sortedResponse = unsortedResponse.sort(function(a, b) {
+  			if (a.stockName < b.stockName) {
+    			return -1;
+  			}
+  			if (a.stockName > b.stockName) {
+    			return 1;
+  			}
+  			return 0;
+			}); 
+      console.log("line 39"); 
+      console.log(sortedResponse); 
+      this.setState({DowJonesArray: sortedResponse});
     })
     .catch(function(response){
+    	console.log(response); // Needs to see what should go here
     });
   }  
 
 render() {
+	
+	var mappedResults = this.state.DowJonesArray.map(function(el){
+		return <div className ="container" key={el.stockName}>	
+				<div className = "panel-body">
+					<h3>  
+						<span className = "label label-info">{el.stockName}</span> 
+					</h3>
+
+					<h3><span className="label label-danger">
+						Trading @ <span className ="badge">$ {el.stockPrice}</span>
+					</span></h3>
+
+					<div className="input-group">
+  						<div className="input-group-btn">
+    						<button type="button" className="btn btn-default">Buy</button>
+  							<button type="button" className="btn btn-default">Trade</button>
+  							<button type="button" className="btn btn-default">Info</button>
+  						</div>
+  						<input type="text" className="form-control"/>
+					</div>
+
+				</div> 
+			   </div>;
+	}); 
+	
     return (
-      <div className = "container">
-        <div className ="row">
-        <div className = "col-md-6">
-          <div className = "panel panel-default">
-            <div className="panel-heading">
-            <h3 className="panel-title text-center">Stocks to Buy </h3>
-            </div>
-          </div>
-        </div>  
-      </div>
-    </div>
+      	<div className = "container">
+      		<br/> 
+      		<br/> 
+       		{mappedResults}
+    	</div>
     );
   }
+
 }
 
 export default Trade;
--- app/config/routes.js
+++ app/config/routes.js
@@ -1,21 +1,22 @@
 import React from "react"; 
-import {Route, Indexroute, Router, browserHistory} from "react-router"; 
+import {Route, IndexRoute, Router, browserHistory} from "react-router"; 
 // import Trade component here 
 // import Portfolio component here 
 import Main from "../components/Main.js"; 
-import Portfolio from "../components/Portfolio.js";
-// import Trade from "../components/Trade.js";
+//import Portfolio from "../components/Portfolio.js";
+import Trade from "../components/Trade.js";
 
 const routes = (
 	<Router history = {browserHistory}>
 
-	<Route path="/react" component = {Main}>
-	</Route> 
+		<Route path="/react" component = {Main}>
+			<IndexRoute component={Trade}/>
+		</Route> 
 
-	<Route path = "/" component = {Main} >
+		<Route path = "/trade" component = {Trade} >
 		{/*<Route path = "/trade" component = {Trade}/>*/}
 		
-	</Route> 
+		</Route> 
 
 	</Router>
 ); 
