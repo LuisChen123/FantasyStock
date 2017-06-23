b63f28e3c8544735cd06dcc90d5d3501e04e0727
Clinton Diamond <clintondiamond@Clinton-Diamonds-MacBook-Pro.local>
2017-06-22 20:17:28 -0700

    updated bugs regarding selling and buying amount

--- models/dbManager.js
+++ models/dbManager.js
@@ -14,15 +14,6 @@ var dbManager = {
 		})
 	},
 
-	// userModel_GetUserInfo: function(cb){
-	// 	userModel.find({}, function(err, info){
-	// 		if(err){
-	// 			console.log(err);
-	// 		}
-	// 		cb(info); 
-	// 	});
-	// },
-
 	userModel_SaveUser: function(req, res) {
 
 		// console.log(req.body+ "this is 18")
@@ -60,45 +51,28 @@ var dbManager = {
 		userModel.find({"username":req.body.UserName}).exec(function(err,result){	
 			if(!result.length){      //both wrong
 				// console.log("working in here");
-				res.send("/logIn");
+				res.send("/");
 			}
 			else{
-			// console.log(result[0].loginPassword + "   49");
-			// console.log(req.body.PassWord + "       52");
+
 			var dbPassword = result[0].loginPassword;
 			console.log(dbPassword + "53");
     		bcrypt.compare(req.body.PassWord, dbPassword, function(err, bcryptRes) {
 				console.log(bcryptRes + "          55");
       			if (bcryptRes) {
-      				// console.log(req.body.UserName, "        line 66");
+
       				res.cookie("SSID", req.body.UserName);
-      				
-      				// console.log(req.cookies, "           dbmanager line65");
-      				// var cookie = req.cookies;
-      				// console.log(cookie.SSID, "                dbmanager line67")
-      				// console.log("dbManager line 65 res ",res);
+
       				res.send('/react');
-      				
-        			
+       			
       			}	 
       			else {
-      				console.log("line 71")
-        			res.send('/logIn');
+        			res.send('/');
 					
       			}
     		});
 			}
 		});
-
-		// var body = req.body;
-		// bcrypt.genSalt(10, function(err, salt) {
-      	// 	bcrypt.hash(body.PassWord, salt, function(err, hash) {
-		// 		  console.log(body.UserName + "  " + hash + " line 67 ");
-		// 		userModel.findOne({"username":body.UserName, "loginPassword": hash},function(err,result){
-		// 			console.log(result + "49");
-		// 		});	
-		// 	});
-  		// });
 	}, 
 
 	userModel_PostTradeUpdate: function(req, res){
@@ -128,70 +102,9 @@ var dbManager = {
     							}
   						 }
   		);
-		// userModel.find({username: username}, function (err, user) {
-  // 		if (err) return handleError(err);
-  // 		user.cash = res.updatedCash;
-  // 		user.stockPortfolio = res.updatedStockPortfolio;
-  // 		user.tradeHistory = res.updatedTradeHistory; 
-  // 		user.save(function (err, updatedUser) {
-  //   		if (err) return handleError(err);
-  //   		res.send(updatedUser);
-  // 			});
-  // 		});
 
 	}
-	/*
-	// will check to see if user has the money to make the purchase, if so then user info will be updated,
-	// if not, user will be alerted. 
-	User_WillTakeCareOfUserRequest: function(userId, shareName, sharePrice, cb){
-		
-		var userId = this.User_GetAll().then( (res) => {
-			if(res.cash>=sharePrice){
-				var updatedCashAmount = res.cash-sharePrice; 
-				userModel.update({cash:{type:Number}})
-				.then(()=>{
-					userModel.insert({tradeHistory: {}}, $push: {tradeHistory: updatedCashAmount})
-					.then((doc)=>{
-						cb(doc);
-					}); 
-				});
-			}
-			else{
-				cb("You Dont Have Enough Cash To Make This Purchase");
-			}
-		});
-	
-	}, 
-	// Users_InsertNew: function(articleID, url, cb){
-	// 	articleModel.create({
-	// 		articleID: articleID, 
-	// 		url: url
-	// 	}, function(err, info){
-	// 		if(err){
-	// 			console.log(err);
-	// 		}
-	// 		cb(info); 
-	// 	}); 
-	// }, 
 
-	// Users_Update: function(id, cb){
-	// 	articleModel.findOneAndUpdate({_id: id}, function(err, info){
-	// 		if(err){
-	// 			console.log(err);
-	// 		}
-	// 		cb(info); 
-	// 	});
-	// },
-
-	// Users_Delete: function(id, cb){
-	// 	articleModel.remove({_id:id}, function(err, info){
-	// 		if(err){
-	// 			console.log(err);
-	// 		}
-	// 		cb(info);
-	// 	});
-	// }
-	*/
 }
 
 module.exports = dbManager; 
