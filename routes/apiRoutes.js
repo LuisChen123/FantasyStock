d9314bfa7db81727e19ff253d48f6249ca945a77
kodakyellow <zi5253hao@gmail.com>
2018-04-07 10:14:11 -0700

    Deleting testing code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,5 @@
-// import dependencies
 var express = require("express");
-// import dbManager 
 var dbManager = require("../models/dbManager.js"); 
-// get an instance of the router object
 var router = new express.Router(); 
 
 router.get("/saved", function(req, res){
@@ -20,21 +17,5 @@ router.post("/joinleague", dbManager.leagueModel_JoinLeague);
 router.get("/league", dbManager.leagueModel_GetUserLeague);
 
 router.post("/update", dbManager.userModel_PostTradeUpdate);
-// router.post("/saved", function(req, res){
-// 	console.log("inside post /saved");
-// 	var articleID = req.body.id;
-// 	var url = req.body.url;
-// 	dbManager.articleModel_InsertNew(articleID, url, function(info){
-// 		console.log(info);
-// 		res.send(info); 
-// 	});
-// });
-
-// router.delete("/saved/:id", function(req, res){
-// 	var id = req.params.id; 
-// 	dbManager.articleModel_Delete(id, function(info){
-// 		res.send(info);
-// 	});
-// }); 
 
 module.exports = router;
