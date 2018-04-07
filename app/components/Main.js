d82f5481d7293800aa18f9d4d6c2728956d929fd
kodakyellow <zi5253hao@gmail.com>
2018-04-07 10:07:14 -0700

    Deleting console logs

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,10 +4,7 @@ import Navbar from "./navbar.js";
 import Footer from "./footer.js";
 
 
-
-// req.cookie.SSID
 class Main extends Component {
-    // Here we render the function
   render() {
     return (
       <div className = "container"> 
